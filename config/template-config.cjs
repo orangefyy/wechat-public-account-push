3457aa941d34123d5b71589f3d2c4913453c406b
orangefyy <109773078+orangefyy@users.noreply.github.com>
2025-02-14 15:22:49 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -4,35 +4,22 @@ const TEMPLATE_CONFIG = [
     id: '0001',
     title: '亲爱的, 早上好',
     desc: `
-      **{{date.DATA}}**
-      
-      下个休息日：{{holidaytts.DATA}}
-      
-      ---
-      
-      城市：{{city.DATA}}
-      
-      天气：{{weather.DATA}}
-      
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      风向: {{wind_direction.DATA}}
-      
-      风级: {{wind_scale.DATA}}
-      
-      {{comprehensive_horoscope.DATA}}
-      
-      ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
+     {
+    id: "0001",
+    title: "姐姐, 情人节快乐",
+    desc: `
+      🗓️{{date.DATA}}
+
+今天是我们在一起的第{{love_day.DATA}}天，爱你❤️
+
+今日{{city.DATA}}天气☁️：{{tian_api_weather_weather_0.DATA}}
+温度🌡️: {{tian_api_weather_lowest_0.DATA}}-{{tian_api_weather_highest_0.DATA}}
+紫外线强度指数☀️: {{tian_api_weather_uv_index_0.DATA}}
+穿衣指数👚: {{tian_api_weather_tips_0.DATA}}
+
+💌{{earthy_love_words.DATA}}
+    `,
+  },
     `
   },
   {
