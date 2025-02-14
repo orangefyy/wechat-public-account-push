03d19d0514ff1229a6f639a38e146cb4c2340eef
orangefyy <109773078+orangefyy@users.noreply.github.com>
2025-02-14 16:03:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ogIrv7I1koNeYeSfr4312q8PhPtY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '5Jmglccg1vUhXEO00tanq39jv1y0SM-ANeKe6Xf4tTw',
+      useTemplateId: '_uaz7PMOUzCbj6UcGJ12wpJ5CZnjLk9d1HOxy93UUNE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-11',
       festivals: [
